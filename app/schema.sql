16c310594c3ff191b52739c8479d8fdd741c7a06
Gilmar San Buenaventura <mac@codesignate.com>
2015-04-05 10:04:43 +0800

    Added foundation js files

--- app/schema.sql
+++ app/schema.sql
@@ -14,9 +14,11 @@ create table logs (
   status smallint not null
 );
 
-insert into users (username, password) values ('mac', '140c1f12feeb2c52dfbeb2da6066a73a');
-insert into users (username, password) values ('alexis', '059bf68f71c80fce55214b411dd2280c');
-insert into users (username, password) values ('jovie', '2240ef71392026698809c0df09c4c694');
+-- USERS 
+insert into users (id, username, password) values (1, 'mac', '140c1f12feeb2c52dfbeb2da6066a73a');
+insert into users (id, username, password) values (2, 'alexis', '059bf68f71c80fce55214b411dd2280c');
+insert into users (id, username, password) values (3, 'jovie', '2240ef71392026698809c0df09c4c694');
 
+-- LOGS
 insert into logs (log_id, username, start_date, end_date, status) values (1, 'mac', '2015-03-30 08:00:00', '2015-03-30 17:00:00', 0);
 insert into logs (log_id, username, start_date, end_date, status) values (2, 'mac', '2015-03-31 08:00:00', '2015-03-31 17:00:00', 0);
